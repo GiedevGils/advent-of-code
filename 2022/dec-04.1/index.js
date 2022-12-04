9d2badf70cf2cda8b0779e6a2a6fd8e081e1e670
Giede van Gils <giedevangils@proton.me>
2022-12-04 13:55:40 +0100

    add 2022-12-04

---
+++ 2022/dec-04.1/index.js
@@ -0,0 +1,24 @@
+const fs = require('fs')
+const readline = require('readline')
+
+const input = readline.createInterface({ input: fs.createReadStream('./input.txt') })
+const isWithinOrEqual = (number, range) => {
+  const [head, tail] = range.split('-')
+  return number >= +head && number <= +tail
+}
+
+let result = 0
+
+input.on('line', line => {
+  const [pair1, pair2] = line.split(',')
+  if (
+    pair1.split('-').every(x => isWithinOrEqual(+x, pair2)) ||
+    pair2.split('-').every(x => isWithinOrEqual(+x, pair1))
+  ) {
+    result++
+  }
+})
+
+input.on('close', () => {
+  console.log(result)
+})
