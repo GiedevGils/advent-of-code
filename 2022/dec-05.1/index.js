11a47df53753d90e00d333c5608cfe55474e4e20
Giede van Gils <giedevangils@proton.me>
2022-12-05 16:06:42 +0100

    add 2022-12-05

---
+++ 2022/dec-05.1/index.js
@@ -0,0 +1,38 @@
+const fs = require('fs')
+const readline = require('readline')
+
+const input = readline.createInterface({ input: fs.createReadStream('./input.txt') })
+
+const stackHolder = []
+const stacks = {}
+let stacksMade = false
+
+const fillStacks = () => {
+  stackHolder.forEach(line => {
+    const boxes = line.match(/.{1,4}|./g).map(x => x.trim())
+    boxes.forEach((box, idx) => {
+      const stack = idx + 1
+      if (box) stacks[stack]?.length ? stacks[stack].unshift(box) : stacks[stack] = [box]
+    })
+  })
+
+  stacksMade = true
+}
+
+input.on('line', line => {
+  const nr = +(line.replaceAll(' ', ''))
+  if (!stacksMade && isNaN(nr)) stackHolder.push(line)
+  if (!stacksMade && !isNaN(nr)) fillStacks()
+
+  const [/**/, amount, /**/, from, /**/, to] = line.split(' ')
+  if (!amount || !from || !to) return
+
+  for (let i = 0; i < amount; i++) {
+    const box = stacks[from].pop()
+    stacks[to].push(box)
+  }
+})
+
+input.on('close', () => {
+  console.log(Object.values({ ...stacks }).map(x => x.pop().replaceAll('[', '').replaceAll(']', '')).join(''))
+})
