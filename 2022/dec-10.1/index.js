d2c1263a648188024a6aace0b631267e1740de1d
PjaKooij <pjakooij@gmail.com>
2022-12-10 14:40:45 +0100

    add 2022-12-10

---
+++ 2022/dec-10.1/index.js
@@ -0,0 +1,38 @@
+const fs = require('fs')
+const readline = require('readline')
+
+const input = readline.createInterface({ input: fs.createReadStream('./input.txt') })
+
+let x = 1
+let commands = []
+let cycle = 0
+let cycleResults = []
+
+const addCycle = () => {
+    cycle += 1
+    if ([20, 60, 100, 140, 180, 220].includes(cycle)) {
+        cycleResults.push(x * cycle)
+        console.log({ cycle, x })
+    }
+}
+
+input.on('line', line => {
+    addCycle()
+    const commandToExecute = commands.shift()
+
+    if (commandToExecute !== undefined) {
+        x += commandToExecute
+        addCycle()
+    }
+
+    const [command, value] = line.split(' ')
+
+    if (command === 'addx') {
+        commands.push(Number(value))
+    }
+})
+
+input.on('close', () => {
+    const result = cycleResults.reduce((total, amount) => total + amount, 0)
+    console.log('finished', { result, cycle })
+})
