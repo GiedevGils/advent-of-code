30d82e94ea810c0d140a9cf2775c712587d8b81c
Giede van Gils <giedevangils@proton.me>
2024-12-06 20:49:32 +0100

    attempt3 dec6p2

--- 2024/12.06/2.js
+++ 2024/12.06/2.js
@@ -14,52 +14,66 @@ input.on('line', line => {
 })
 
 input.on('close', () => {
-  console.time('total')
+  console.time('total duration')
 
   let possibleBlockages = 0
+  const blockedPositions = new Set()
 
-  for (let idxX = 0; idxX < grid.length; idxX++) {
-    console.timeLog('total')
-    const line = grid[idxX]
+  const { getSingle } = makeGridTraverser(grid)
+  let { posX, posY } = findCursor(grid)
 
-    for (let idxY = 0; idxY < line.length; idxY++) {
-      const element = line[idxY]
+  let direction = 'above'
 
-      if (element === '#') continue
+  while (direction) {
+    const { newIdxX, newIdxY, value } = getSingle(direction)(posX, posY)
+
+    if (!value) direction = null
+
+    if (value === '#') {
+      direction = getNextDirection(direction)
+    } else {
+      posX = newIdxX
+      posY = newIdxY
 
       const newGrid = R.clone(grid)
+      if (value) newGrid[newIdxX][newIdxY] = 'O'
 
-      newGrid[idxX][idxY] = 'O'
-
-      const isFinite = isGridFinite(grid, newGrid)
-
-      if (!isFinite) possibleBlockages++
+      if (!isGridFinite(grid, newGrid)) {
+        blockedPositions.add(`${newIdxX},${newIdxY}`)
+        possibleBlockages = blockedPositions.size
+      }
     }
   }
 
   // 13 niet correct <-- incorrect input
   // 14753 too high
+  // 3494 not right
+  // 3495 not right
+  // 3647 not right
   console.log({ possibleBlockages })
-  console.timeEnd('total')
+  console.timeEnd('total duration')
 })
 
 function isGridFinite (grid, newGrid) {
-  const { getSingle, getAll } = makeGridTraverser(newGrid)
+  const visitedDirectionPositions = new Set()
+  const { getSingle } = makeGridTraverser(newGrid)
   let { posX, posY } = findCursor(grid)
+
   let direction = 'above'
 
-  let hasNoticedO = false
-
-  setInterval(() => {
-    console.log(direction)
-  }, 500)
-
   while (direction) {
+    // if the cursor has already passed ovre this location with this specific direction
+    // it is stuck in a loop
+    if (visitedDirectionPositions.has(`${direction}${posX}${posY}`)) {
+      return false
+    }
+
+    visitedDirectionPositions.add(`${direction}${posX}${posY}`)
+
     const { newIdxX, newIdxY, value } = getSingle(direction)(posX, posY)
+
     const newDirection = getNextDirection(direction)
 
-    if (hasNoticedO && value === 'O' && getAll(newDirection)(posX, posY).includes('#')) return false
-    if (!hasNoticedO && value === 'O') hasNoticedO = true
     if (!value) return true
 
     if (value === '#' || value === 'O') {
